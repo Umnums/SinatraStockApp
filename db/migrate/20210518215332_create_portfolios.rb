f7c003a76de6b24d406ab35e51e62e8bcc14638b
joseph <josephtkao@gmail.com>
2021-05-24 22:11:51 -0500

    added stocks

--- db/migrate/20210518215332_create_portfolios.rb
+++ db/migrate/20210518215332_create_portfolios.rb
@@ -1,5 +1,11 @@
 class CreatePortfolios < ActiveRecord::Migration
   def change
     create_table :portfolios do |t|
+      t.string name
+      t.string description
+      t.integer starting
+      t.integer current
+      
+    end
   end
 end
---
+++ db/migrate/20210524203408_create_stocks.rb
@@ -0,0 +1,11 @@
+class CreateStocks < ActiveRecord::Migration
+  def change
+    create_table :stocks do |t|
+      t.string name
+      t.string symbol
+      t.integer current_price
+      
+
+    end
+  end
+end
